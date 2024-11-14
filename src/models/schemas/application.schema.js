4b37d39a7beffc63d793f5dc626c80d1a4fc9746
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2024-11-14 10:39:52 +0530

    (refactor)Refactored code to seperate out intent #15

--- src/models/schemas/application.schema.js
+++ src/models/schemas/application.schema.js
@@ -2,31 +2,6 @@
  * @swagger
  * components:
  *   schemas:
- *     Intent:
- *       type: object
- *       description: An intent definition
- *       required:
- *         - name
- *       properties:
- *         name:
- *           type: string
- *           description: The name of the intent
- *           example: "ViewChart"
- *         displayName:
- *           type: string
- *           description: An optional display name for the intent
- *           example: "View Trading Chart"
- *         contexts:
- *           type: array
- *           items:
- *             type: string
- *           description: The contexts the intent accepts (typically namespaced context types)
- *           example: ["org.fdc3.instrument", "org.symphony.contact"]
- *         customConfig:
- *           type: object
- *           description: Custom configuration for the intent required by a particular desktop agent
- *           example: { "agent": "symphony", "version": "2.0" }
- *
  *     Application:
  *       type: object
  *       required:
@@ -53,7 +28,7 @@
  *         intents:
  *           type: array
  *           items:
- *             $ref: '#/components/schemas/Intent'
+ *             $ref: './intent.schema.js'
  *           description: List of intents supported by the application
  *           example:
  *             - name: "ViewChart"
